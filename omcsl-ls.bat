62319a7f35215237c3f49ac7d501c49cefa472eb
雪リリ <yukinshell@qq.com>
2024-11-05 13:01:01 +0800

    更新进度

--- omcsl-ls.bat
+++
@@ -1,7 +0,0 @@
-@echo off & chcp 65001 >nul & setlocal enabledelayedexpansion
-::cmd arg: <jar> <Xmx>
-::env arg: [JAVA_BIN]
-
-
-set OMCSL_YGGDRASIL_FLAGS=%~dp0\flags\yggdrasil.txt
-%~dp0\omcsl %1 %2
