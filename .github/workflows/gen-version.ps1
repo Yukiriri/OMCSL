1d4ed7647385f4cc458d9af34a6e58f5a1aee1c9
illuminula <50292962+illuminula@users.noreply.github.com>
2026-09-08 19:11:03 +0800

    更新workflow

--- .github/workflows/gen-version.ps1
+++ .github/workflows/gen-version.ps1
@@ -1,6 +1,6 @@
 $date = Get-Date -Format 'yyyy.MM.dd'
-$bin_ver     = "$date.0"
-$release_ver = "$date+$env:GITHUB_RUN_NUMBER"
+$bin_ver     = "$date.{0:D2}" -f ($env:GITHUB_RUN_NUMBER % 100)
+$release_ver = "$date+{0:D2}" -f ($env:GITHUB_RUN_NUMBER % 100)
 
 Write-Output bin_ver=$bin_ver
 Write-Output release_ver=$release_ver
